7409dc235b9fd00fd081a53affa9c0fbfb11f978
Harish Mhatre <harish@promobitech.com>
2019-01-07 19:31:46 +0530

    Update program.java

--- program.java
+++ program.java
@@ -5,7 +5,7 @@ class program {
     public static void main(String args[])
     {
 
-        System.out.println("Test");
+        system.out.println("Test");
         //int a=10,b=20,c;
         //c=a+b;
 
